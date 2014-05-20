9dcf873c3b1e496a2ad56345a125a0a9615e23bb
evangelion62 <sebastien.ronger.evangelion@gmail.com>
2014-05-20 17:42:03 +0200

    page d'acceuil BO

--- wireframe/BO/index.html.php
+++ wireframe/BO/index.html.php
@@ -1,6 +1,14 @@
 <?php require_once 'layout/haut.html.php';?>
 
 <section>
+	<ul>
+		<li>ressouces</li>
+		<li>ticket</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
 </section>
 
 <?php require_once 'layout/bas.html.php';?>
