c7ef8831017d14a072a832f77a20c783089dae90
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-05-26 15:56:12 +0200

    Update index.html.php

--- wireframe/BO/index.html.php
+++ wireframe/BO/index.html.php
@@ -1,14 +1,3 @@
 <?php require_once 'layout/haut.html.php';?>
 
-<section>
-	<ul>
-		<li>ressouces</li>
-		<li>ticket</li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-</section>
-
 <?php require_once 'layout/bas.html.php';?>
