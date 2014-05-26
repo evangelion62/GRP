3f2e2097900f153b0094ff7c436c82b180da574c
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-05-26 15:55:15 +0200

    Update haut.html.php

--- wireframe/BO/layout/haut.html.php
+++ wireframe/BO/layout/haut.html.php
@@ -1,8 +1,36 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
 		<meta charset="UTF-8"/>
-		<title>BO Wireframe</title>
-		<link rel="stylesheet" href="css/style.css">
+		<title>GRP - Réseau</title>
+		<link rel="stylesheet" href="style.css"/>
 	</head>
+	
 	<body>
+		<header>
+			<h1>GRP</h1>
+			<nav>
+				<ul>
+					<li ><a href="index.html"><span class="glyphicon glyphicon-home"></span><br/>Réseau</a></li>
+					<li><a href="user.html"><span class="glyphicon glyphicon-user"></span><br/>Utilisateurs</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-tags"></span><br/>Tikets</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-book"></span><br/>Doc</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-cog"></span><br/>Paramétres</a></li>
+				<ul>
+			</nav>
+		</header>
+		
+		<nav class="verticalNav">
+			<ul>
+				<li><span class="glyphicon glyphicon-search"></span>ressources materiels</li>
+				<hr/>
+				<li><span class="glyphicon glyphicon-plus-sign"></span>ajouter</li>
+				<hr/>
+				<li><span class="glyphicon glyphicon-pencil"></span>modifier</li>
+				<hr/>
+				<li><span class="glyphicon glyphicon-trash"></span>suprimer</li>
+				<hr/>
+			</ul>
+		</nav>
+		<section >
