94623627d5afbb016857281ca4787fb17be4f71d
evangelion <sebastien.ronger.evangelion@gmail.com>
2014-05-28 00:38:49 +0200

    css BO

--- wireframe/BO/index.html.php
+++ wireframe/BO/index.html.php
@@ -1,5 +1,8 @@
 <?php 
 $pageTitle = "GRP - Réseau";
 require_once 'layout/haut.html.php';?>
-
+<form action="pageDeTraitement.php" method="post">
+	<label for=""></label><input id="" name="" type="text" value="test">
+	<input>
+</form>
 <?php require_once 'layout/bas.html.php';?>
