eb78a3dac3cd7c92ec9f3fffcafbfdb9f73afdf5
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-06-02 16:42:05 +0200

    css form BO
    mise en place du css pour les formulaire du BO

--- wireframe/BO/index.html.php
+++ wireframe/BO/index.html.php
@@ -1,8 +1,15 @@
 <?php 
 $pageTitle = "GRP - Réseau";
 require_once 'layout/haut.html.php';?>
+<h2>
+	test de titre2
+</h2>
 <form action="pageDeTraitement.php" method="post">
-	<label for=""></label><input id="" name="" type="text" value="test">
-	<input>
+	<label for=""><span class="glyphicon glyphicon-home"></span></label><input id="" name="" type="text" value="test">
+	<input id="" name="" type="text" value="test">
+	<input class="btn" type="submit" value="Envoyer">
 </form>
+<p>
+	test de paragraphe
+</p>
 <?php require_once 'layout/bas.html.php';?>
