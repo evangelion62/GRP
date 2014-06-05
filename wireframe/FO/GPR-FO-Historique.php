12004fb2c0c6b7ed15880ee3d20b2df55c5c9d98
docdoxey <vbvincentboulanger@gmail.com>
2014-06-05 17:27:46 +0200

    creation menu et ajout des pages
    ajout page ticket en cours / ajout de l'historique

---
+++ wireframe/FO/GPR-FO-Historique.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/style.css"/>
+		<title>GPR-FO-Historique</title>
+	</head>
+	<body>
+		<header>
+			<h2>Historique des tickets</h2>
+				<ol>
+					<li>Liste</li>
+					<li>liste 1</li>
+					<li>Liste 2</li>
+					<li>Liste 3</li>
+					<li>liste 4</li>
+					<li>Liste 5</li>
+					<li>Liste 6</li>
+					<li>liste 7</li>
+					<li>Liste 8</li>
+				</ol>
+		</header>
+	</body>
+</html>
---
+++ wireframe/FO/GPR-FO-Ticket-en-cours.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/style.css"/>
+		<title>GPR-FO-Ticket-en-cours</title>
+	</head>
+	<body>
+		<header>
+			<h2>Tickets en cours</h2>
+				<ul>
+					<li>tableau</li>
+					<li>tableau 1</li>
+					<li>tableau 2</li>
+				</ul>
+		</header>
+	</body>
+</html>
