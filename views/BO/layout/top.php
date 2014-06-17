17be694e15083ef10c531ef4c3278f2867a29878
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-06-17 13:37:40 +0200

    views BO layout

--- views/BO/layout/top.php
+++ views/BO/layout/top.php
@@ -2,7 +2,12 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title><?php $title;?></title>
+		<title><?php echo $title;?></title>
+		<link rel="stylesheet" href=<?php echo $stylesheet;?>>
 	</head>
 	<body>
-	
+		<?php require_once 'views/BO/layout/menu.top.php';?>
+
+		<?php require_once 'GRP/views/BO/layout/menu.left.php';?>
+		
+		<?php echo $content;?>
