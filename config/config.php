8ce5ec8cb99a770fcf1e984716e7fe9a5615bb2f
docdoxey <vbvincentboulanger@gmail.com>
2014-06-17 12:04:53 +0200

    Connection à la base de données

--- config/config.php
+++ config/config.php
@@ -1,1 +1,8 @@
 <?php
+//Connection à la base de données // 
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=GRP', 'grpuser', 'grp', array());
+} catch (PDOException $e) {
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
+}
