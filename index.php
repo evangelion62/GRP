0743bab66e7c62a47c67e72fba6895cfc3968a89
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-06-17 13:18:59 +0200

    front controller
    et debut d'implemantation du controller error

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 require_once 'config/config.php';
+
+//chargement du sub controller
+if (is_file($controller)) {
+	require_once $controller;
+}else{
+	require_once 'controllers/error.php';
+}
---
+++ views/BO/error/404.php
@@ -0,0 +1,3 @@
+<?php require_once 'views/BO/layout/top.php';?>
+
+<?php require_once 'views/BO/layout/bot.php';?>
