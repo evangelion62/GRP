1521748e8d2b4daa78c0d33600ebeebf6074aa66
evangelion <sebastien.ronger.evangelion@gmail.com>
2014-06-18 22:18:17 +0200

    indentation et mise en forme du fichier

--- config/config.php
+++ config/config.php
@@ -1,27 +1,37 @@
 <?php
-//Connection à la base de données // 
-try {
+/*
+ * Connection à la base de données
+ * */ 
+try 
+{
 	$bdd = new PDO('mysql:host=localhost;dbname=GRP', 'grpuser', 'grp', array());
-} catch (PDOException $e) {
+}
+catch (PDOException $e)
+{
 	print "Error!: " . $e->getMessage() . "<br/>";
 	die();
 }
 
-//definition de la variable controller en fonction du paramètre reçu en http par methode GET//
-if (isset ($_GET['controller']) && !empty ($_GET['controller']) && strlen($_GET['controller']) <= 254 ) {
-	$controller = $_GET['controller'];
-	
+
+/*
+ * definition de la variable controller en fonction du paramètre reçu en http par methode GET
+ */
+if (isset ($_GET['controller']) && !empty ($_GET['controller']) && strlen($_GET['controller']) <= 254 ) 
+{
+	$controller = $_GET['controller'];	
 }
-else{
+else
+{
 	$controller= "error";	
 }
-
-//definition de la varible action en fonction du paramètre reçu en http par methode GET// 
-if(false){
+/*
+ * definition de la varible action en fonction du paramètre reçu en http par methode GET
+ */ 
+if(false)
+{
 	$action= $_GET['action'];
-	
 }
-else{
-	
-	$action= "error"; 
+else
+{	
+	$action= "error";
 }
