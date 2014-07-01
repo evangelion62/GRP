4b2c4a3ce670b07e95e6d64134dc58dac5d43880
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-07-01 11:06:11 +0200

    implemantation de l'entité user

--- models/user.php
+++ models/user.php
@@ -3,6 +3,28 @@
  * classe entité User 
  */
 
-class User {
+class User extends Entity{
+	//attribus
+	private $_userName;
+	private $_pwd;
 	
+	//getters
+	public function userName(){
+		return $this->_userName;
+	}
+	
+	public function pwd(){
+		return $this->_pwd;
+	}
+	
+	//setters
+	public function setUserName($userName){
+		 $userName = (string) $userName;
+		 
+		 
+	}
+	
+	public function setPwd($pwd){
+	
+	}
 }
