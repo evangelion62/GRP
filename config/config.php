642bba38c901f92dc4d18b60f204de1cdf00475e
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-07-01 11:03:55 +0200

    ajout de l'autoload au fichier de config

--- config/config.php
+++ config/config.php
@@ -16,22 +16,35 @@ catch (PDOException $e)
 /*
  * definition de la variable controller en fonction du paramètre reçu en http par methode GET
  */
-if (isset ($_GET['controller']) && !empty ($_GET['controller']) && strlen($_GET['controller']) <= 254 ) 
+if (isset ($_GET['controller']) && !empty ($_GET['controller']) && strlen($_GET['controller']) <= 254 && is_string($_GET['controller'])) 
 {
-	$controller = $_GET['controller'];	
+	$_GET['controller'] = htmlspecialchars($_GET['controller']);
+	$controller = 'controllers/'.$_GET['controller'].'.php';	
 }
 else
 {
-	$controller= "error";	
+	$controller= "controllers/error.php";	
 }
 /*
  * definition de la varible action en fonction du paramètre reçu en http par methode GET
  */ 
-if(false)
+if(isset ($_GET['action']) && !empty ($_GET['action']) && strlen($_GET['action']) <= 254 )
 {
+	$_GET['action'] = htmlspecialchars($_GET['action']);
 	$action= $_GET['action'];
 }
 else
 {	
 	$action= "error";
 }
+
+/*
+ * auto chargement des fichier classe 
+ */
+function chargerClasse($classe)
+{
+	if (is_file('models/'.$classe . '.php')){
+		require 'models/'.$classe . '.php'; // On inclut la classe correspondante au paramètre passé.
+	}
+}
+spl_autoload_register('chargerClasse');
